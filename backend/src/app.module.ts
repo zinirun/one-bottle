c46f2a42a44ded23039ac7fd0fd6114b2e857acb
zinirun <heojj97@naver.com>
2021-04-25 21:16:43 +0900

    modify: mapping service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { DateScalar } from './scalars/date';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot(),
+        GraphQLModule.forRoot({
+            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+            context: ({ req }) => ({ req }),
+            cors: {
+                credentials: true,
+                origin: true,
+            },
+        }),
+        AuthModule,
+        UserModule,
+    ],
+    providers: [DateScalar],
 })
 export class AppModule {}
