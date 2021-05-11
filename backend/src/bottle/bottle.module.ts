30a42cb681e4143d8b8c131b3ee7ca14f66d1b04
zinirun <heojj97@naver.com>
2021-05-11 20:52:45 +0900

    modify: BottleModule

--- backend/src/bottle/bottle.module.ts
+++ backend/src/bottle/bottle.module.ts
@@ -2,8 +2,11 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from 'src/user/user.module';
 import { Bottle } from './bottle.entity';
+import { BottleMember } from './bottle.member.entity';
+import { BottleService } from './bottle.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Bottle]), forwardRef(() => UserModule)],
+    imports: [TypeOrmModule.forFeature([Bottle, BottleMember]), forwardRef(() => UserModule)],
+    providers: [BottleService],
 })
 export class BottleModule {}
