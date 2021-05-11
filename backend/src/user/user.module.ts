50fa9061c0c008aafbd4e13ab021eb9479b6fb9d
zinirun <heojj97@naver.com>
2021-05-11 21:47:01 +0900

    modify: UserModule imports

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -4,14 +4,9 @@ import { User } from './user.entity';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 import { AuthModule } from 'src/auth/auth.module';
-import { BottleModule } from 'src/bottle/bottle.module';
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([User]),
-        forwardRef(() => AuthModule),
-        forwardRef(() => BottleModule),
-    ],
+    imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
     providers: [UserResolver, UserService],
     exports: [UserService],
 })
