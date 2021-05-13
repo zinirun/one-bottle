c65972d3d17a7a516f147eea439dff00a3d4e9d2
zinirun <heojj97@naver.com>
2021-05-13 20:59:06 +0900

    modify: thread

--- backend/src/thread/thread.resolver.ts
+++ backend/src/thread/thread.resolver.ts
@@ -1,4 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { LoginGuard } from 'src/guards/login.guard';
+import { Thread } from './thread.entity';
+import { ThreadService } from './thread.service';
 
 @Resolver()
-export class ThreadResolver {}
+export class ThreadResolver {
+    constructor(private readonly threadService: ThreadService) {}
+
+    @UseGuards(LoginGuard)
+    @Query(() => Thread)
+    getThread(@Args('id') id: string): Promise<Thread> {
+        return this.threadService.getOne(id);
+    }
+}
--- backend/src/thread/thread.service.ts
+++ backend/src/thread/thread.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Thread } from './thread.entity';
 
 @Injectable()
-export class ThreadService {}
+export class ThreadService {
+    constructor(
+        @InjectRepository(Thread)
+        private readonly threadRepository: Repository<Thread>,
+    ) {}
+
+    public async getOne(id: string): Promise<Thread> {
+        return this.threadRepository.findOne(id);
+    }
+}
