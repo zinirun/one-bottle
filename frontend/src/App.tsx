25c8b2e9cf136ea9fd8596bfd816c29be47e0c8c
zinirun <heojj97@naver.com>
2021-05-16 21:55:27 +0900

    modify: set recoil, routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import { ApolloProvider } from '@apollo/react-hooks';
 import client from '@/graphql/client';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import LoginPage from './views/pages/LoginPage';
+import TeamListPage from './views/pages/TeamListPage';
 
 export default function App() {
     return (
         <ApolloProvider client={client}>
-            <div></div>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={LoginPage} />
+                    <Route path="workspace" component={TeamListPage} />
+                </Switch>
+            </Router>
         </ApolloProvider>
     );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 
 ReactDOM.render(
-    <React.StrictMode>
+    <RecoilRoot>
         <App />
-    </React.StrictMode>,
+    </RecoilRoot>,
     document.getElementById('root'),
 );
